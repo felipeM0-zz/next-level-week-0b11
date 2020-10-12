1964d5b98a3a82282a5d71078c32094cf2f235fb
Felipe Moreira <felipemoreira.97@hotmail.com>
2020-10-12 13:52:20 -0300

    Criado layout landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+
+import logoImg from "./images/logo.svg";
+import { FiArrowRight } from "react-icons/fi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>São Luís</strong>
+          <span>Maranhão</span>
+        </div>
+
+        <a href="https://google.com.br" className="enter-app">
+          <FiArrowRight size={26} color="#8d734b" />
         </a>
-      </header>
+      </div>
     </div>
   );
 }
