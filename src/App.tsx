2c2a8e63bf5ebaec39f664593a5a1966e5c146a1
Felipe Moreira <felipemoreira.97@hotmail.com>
2020-10-12 16:05:28 -0300

    Criado a página OrphanatesMap e seu layout; Incluído o mapaá página

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
 import React from "react";
 
 import "./styles/global.css";
-import "./styles/pages/landing.css";
 
-import logoImg from "./images/logo.svg";
-import { FiArrowRight } from "react-icons/fi";
+import Routes from "./routes";
 
 function App() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>São Luís</strong>
-          <span>Maranhão</span>
-        </div>
-
-        <a href="https://google.com.br" className="enter-app">
-          <FiArrowRight size={26} color="#8d734b" />
-        </a>
-      </div>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
